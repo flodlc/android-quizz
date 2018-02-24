25b4fd53054c9d49a0ab196ad5d94ed4f56b5842
Florian DE LA COMBLE <f.delacomble@gmail.com>
2018-02-24 15:34:56 +0100

    v1.1.7 remove logout

--- android-quizz/app/src/main/java/com/quizz/HomeActivity.java
+++ android-quizz/app/src/main/java/com/quizz/HomeActivity.java
@@ -35,26 +35,5 @@ public class HomeActivity extends AppCompatActivity {
 
         getSupportFragmentManager().beginTransaction().add(R.id.content, on, "ONLINE").commit();
         getSupportFragmentManager().beginTransaction().add(R.id.content, off, "OFFLINE").commit();
-
-        findViewById(R.id.logout).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                ApiServiceInterface apiService = ApiService.getService();
-                Call<Boolean> call = apiService.logout();
-                call.enqueue(new Callback<Boolean>() {
-                    @Override
-                    public void onResponse(Call<Boolean> call, Response<Boolean> response) {
-                        deleteFile("QEUser");
-                        RouterService.goConnectPageAndFinish(HomeActivity.this);
-                    }
-
-                    @Override
-                    public void onFailure(Call<Boolean> call, Throwable t) {
-                        deleteFile("QEUser");
-                        RouterService.goConnectPageAndFinish(HomeActivity.this);
-                    }
-                });
-            }
-        });
     }
 }
